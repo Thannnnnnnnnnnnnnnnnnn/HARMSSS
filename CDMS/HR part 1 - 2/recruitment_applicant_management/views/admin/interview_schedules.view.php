416065b4e5bb0f178b979bd66ba0e78fb52241f7
Jeyyymsss <caburian0929@gmail.com>
2025-05-24 12:58:33 +0800

    ulan kolet

--- CDMS/HR part 1 - 2/recruitment_applicant_management/views/admin/interview_schedules.view.php
+++ CDMS/HR part 1 - 2/recruitment_applicant_management/views/admin/interview_schedules.view.php
@@ -1,10 +1,10 @@
-<?php require '../partials/admin/head.php' ?>
+<?php require '../../partials/admin/head.php' ?>
 
 <div class="flex min-h-screen w-full">
     <div class="sidebar-overlay" id="sidebar-overlay"></div>
-    <?php require '../partials/admin/sidebar.php' ?>
+    <?php require '../../partials/admin/sidebar.php' ?>
     <div class="main w-full bg-[#FFF6E8] md:ml-[320px]">
-        <?php require '../partials/admin/navbar.php' ?>
+        <?php require '../../partials/admin/navbar.php' ?>
         <main class="px-8 py-8">
             <?php if ($updated ?? '' === true) : ?>
                 <div role="alert" class="alert alert-success mb-5">
@@ -223,4 +223,4 @@
         });
     })
 </script>
-<?php require '../partials/admin/footer.php' ?>
+<?php require '../../partials/admin/footer.php' ?>
--- CDMS/HR part 1 - 2/recruitment_applicant_management/views/admin/job-offers.view.php
+++ CDMS/HR part 1 - 2/recruitment_applicant_management/views/admin/job-offers.view.php
@@ -1,11 +1,11 @@
-<?php require 'partials/admin/head.php' ?>
+<?php require '../../partials/admin/head.php' ?>
 
 <div class="flex min-h-screen w-full">
     <div class="sidebar-overlay" id="sidebar-overlay"></div>
-    <?php require 'partials/admin/sidebar.php' ?>
+    <?php require '../../partials/admin/sidebar.php' ?>
 
     <div class="main w-full bg-[#FFF6E8] md:ml-[320px]">
-        <?php require 'partials/admin/navbar.php' ?>
+        <?php require '../../partials/admin/navbar.php' ?>
         <main class="px-8 py-8">
             <div>
                 <h2>Accepted Offers</h2>
@@ -72,4 +72,4 @@
     </div>
 </div>
 
-<?php require 'partials/admin/footer.php' ?>
+<?php require '../../partials/admin/footer.php' ?>
